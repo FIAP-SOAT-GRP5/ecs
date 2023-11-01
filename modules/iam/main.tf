437bdbcf208904739995175b63d1c5e812c4b0db
Leandro <leandro.santana@outlook.com>
2023-11-01 10:57:07 -0300

    Alterando arquivo CI/CD e modulo iam

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_policy" "this" {
-    name_prefix = "app_policy"
-    policy = data.aws_iam_policy_document.this.json
-}
-
 resource "aws_iam_role" "aws_role" {
   name = var.aws_iam_role
   description = "Role de Servico do ECS."
